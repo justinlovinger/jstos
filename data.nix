9e168ebdf1a0a8f05c73e4250b25d571051b3bbc
Justin Lovinger <git@justinlovinger.com>
2026-05-27 21:25:58 +0000

    jstos/data: refactor

--- data.nix
+++ data.nix
@@ -5,8 +5,8 @@
   ...
 }:
 let
-  syncCfgs = lib.mapAttrs (_: cfg: cfg.data.sync) config.jstos.users;
-  snapshotCfgs = lib.mapAttrs (_: cfg: cfg.data.snapshot) config.jstos.users;
+  syncCfgs = map (jstos: jstos.data.sync) (builtins.attrValues config.jstos.users);
+  snapshotCfgs = lib.mapAttrs (_: jstos: jstos.data.snapshot) config.jstos.users;
 
   sshAliveInterval = 10;
 in
@@ -77,145 +77,140 @@ lib.mkMerge [
     ];
   }
 
-  (lib.mkIf
-    (builtins.any (cfg: cfg.client.enable || cfg.server.enable) (builtins.attrValues syncCfgs))
-    {
-      # Unison needs many inotify watches.
-      boot.kernel.sysctl."fs.inotify.max_user_watches" = "2147483647";
+  (lib.mkIf (builtins.any (cfg: cfg.client.enable || cfg.server.enable) syncCfgs) {
+    # Unison needs many inotify watches.
+    boot.kernel.sysctl."fs.inotify.max_user_watches" = "2147483647";
 
-      # This is the other half of `unison.sshargs`.
-      services.openssh.settings =
-        lib.mkIf (builtins.any (cfg: cfg.server.enable) (builtins.attrValues syncCfgs))
-          {
-            TCPKeepAlive = "no";
-            ClientAliveInterval = sshAliveInterval;
-          };
+    # This is the other half of `unison.sshargs`.
+    services.openssh.settings = lib.mkIf (builtins.any (cfg: cfg.server.enable) syncCfgs) {
+      TCPKeepAlive = "no";
+      ClientAliveInterval = sshAliveInterval;
+    };
 
-      home-manager.users = lib.mapAttrs (
-        user: jstos:
-        let
-          cfg = jstos.data.sync;
-        in
-        { config, ... }:
-        lib.mkMerge [
-          (lib.mkIf (cfg.client.enable || cfg.server.enable) {
-            home.packages = [ pkgs.unison ];
-          })
+    home-manager.users = lib.mapAttrs (
+      user: jstos:
+      let
+        cfg = jstos.data.sync;
+      in
+      { config, ... }:
+      lib.mkMerge [
+        (lib.mkIf (cfg.client.enable || cfg.server.enable) {
+          home.packages = [ pkgs.unison ];
+        })
 
-          (lib.mkIf cfg.client.enable {
-            services.unison = {
-              enable = true;
-              pairs = {
-                data = {
-                  roots = [
-                    jstos.data.root
-                    "ssh://${cfg.address}/${cfg.root}"
-                  ];
-                  stateDirectory = "${config.home.homeDirectory}/.unison"; # Home Manager uses a different default than upstream.
-                  commandOptions = {
-                    # Unison doesn't sync most metadata by default.
-                    group = "true";
-                    owner = "true";
-                    perms = "-1";
-                    times = "true";
-                    acl = "true";
-                    xattrs = "true";
+        (lib.mkIf cfg.client.enable {
+          services.unison = {
+            enable = true;
+            pairs = {
+              data = {
+                roots = [
+                  jstos.data.root
+                  "ssh://${cfg.address}/${cfg.root}"
+                ];
+                stateDirectory = "${config.home.homeDirectory}/.unison"; # Home Manager uses a different default than upstream.
+                commandOptions = {
+                  # Unison doesn't sync most metadata by default.
+                  group = "true";
+                  owner = "true";
+                  perms = "-1";
+                  times = "true";
+                  acl = "true";
+                  xattrs = "true";
 
-                    # `copyonconflict` combined with `prefer newer`
-                    # allows conflicts to be automatically resolved safely.
-                    copyonconflict = "true";
-                    prefer = "newer";
+                  # `copyonconflict` combined with `prefer newer`
+                  # allows conflicts to be automatically resolved safely.
+                  copyonconflict = "true";
+                  prefer = "newer";
 
-                    # Locks get in the way of automatic restarts,
-                    # and systemd already ensures only one Unison sync per pair runs at a time.
-                    ignorelocks = "true";
+                  # Locks get in the way of automatic restarts,
+                  # and systemd already ensures only one Unison sync per pair runs at a time.
+                  ignorelocks = "true";
 
-                    # By default,
-                    # Unison fails to notice when the remote server stopped sending.
-                    # `TCPKeepAlive` is a legacy option that rarely works
-                    # but is enabled by default.
-                    sshargs = "-o TCPKeepAlive=no -o ServerAliveInterval=${toString sshAliveInterval}";
+                  # By default,
+                  # Unison fails to notice when the remote server stopped sending.
+                  # `TCPKeepAlive` is a legacy option that rarely works
+                  # but is enabled by default.
+                  sshargs = "-o TCPKeepAlive=no -o ServerAliveInterval=${toString sshAliveInterval}";
 
-                    # BTRFS snapshots should not be synced.
-                    ignore = "BelowPath .snapshots";
+                  # BTRFS snapshots should not be synced.
+                  ignore = "BelowPath .snapshots";
 
-                    # Unison generates _many_ `[... blob data]` messages
-                    # without `terse`.
-                    # They are verbose
-                    # and of little value.
-                    terse = "true";
+                  # Unison generates _many_ `[... blob data]` messages
+                  # without `terse`.
+                  # They are verbose
+                  # and of little value.
+                  terse = "true";
 
-                    # As of 2025-02-27,
-                    # Unison may crash while logging.
-                    silent = "true";
-                  };
+                  # As of 2025-02-27,
+                  # Unison may crash while logging.
+                  silent = "true";
                 };
               };
             };
+          };
 
-            # The Unison services take too long to restart.
-            systemd.user.timers.unison-pair-data = lib.mkForce { };
-            systemd.user.services.unison-pair-data = {
-              Install.WantedBy = [ "default.target" ];
-              Service =
-                let
-                  timestampFile = ''$"($env.XDG_RUNTIME_DIR)/unison-pair-data-timestamp"'';
-                in
-                {
-                  Restart = "always";
-                  RestartSec = "1s";
-                  RestartSteps = 10;
-                  RestartMaxDelaySec = "5min";
-                  # Systemd doesn't automatically reset the restart delay
-                  # because Unison never exits sucessfully.
-                  # There's no easy wasy to tell if Unison is fully started,
-                  # so we use a timeout instead.
-                  ExecStartPost = pkgs.writeScript "reset-backoff-start.nu" ''
-                    #!${lib.getExe pkgs.nushell}
-                    date now | save -f ${timestampFile}
-                  '';
-                  ExecStopPost = pkgs.writeScript "reset-backoff.nu" ''
-                    #!${lib.getExe pkgs.nushell}
-                    try {
-                      let start = (open ${timestampFile})
-                      rm ${timestampFile}
-                      if ((date now) - ($start | into datetime) > 60sec) {
-                        ${lib.getExe' pkgs.systemd "systemctl"} --user reset-failed unison-pair-data
-                      }
+          # The Unison services take too long to restart.
+          systemd.user.timers.unison-pair-data = lib.mkForce { };
+          systemd.user.services.unison-pair-data = {
+            Install.WantedBy = [ "default.target" ];
+            Service =
+              let
+                timestampFile = ''$"($env.XDG_RUNTIME_DIR)/unison-pair-data-timestamp"'';
+              in
+              {
+                Restart = "always";
+                RestartSec = "1s";
+                RestartSteps = 10;
+                RestartMaxDelaySec = "5min";
+                # Systemd doesn't automatically reset the restart delay
+                # because Unison never exits sucessfully.
+                # There's no easy wasy to tell if Unison is fully started,
+                # so we use a timeout instead.
+                ExecStartPost = pkgs.writeScript "reset-backoff-start.nu" ''
+                  #!${lib.getExe pkgs.nushell}
+                  date now | save -f ${timestampFile}
+                '';
+                ExecStopPost = pkgs.writeScript "reset-backoff.nu" ''
+                  #!${lib.getExe pkgs.nushell}
+                  try {
+                    let start = (open ${timestampFile})
+                    rm ${timestampFile}
+                    if ((date now) - ($start | into datetime) > 60sec) {
+                      ${lib.getExe' pkgs.systemd "systemctl"} --user reset-failed unison-pair-data
                     }
-                  '';
+                  }
+                '';
 
-                  # Syncing on a metered connection could be expensive.
-                  # See <https://github.com/jdorel/systemd-metered-connection-dependency/blob/4a083159e20b444ac44340884d9563244ba91f7e/check-metered-connection.sh>
-                  # for `check-metered-connection.sh`.
-                  # Technically,
-                  # this only stops the service from _starting_ while on a metered connection.
-                  # In practice,
-                  # the service should fail and restart
-                  # when the connection changes.
-                  # Note,
-                  # an exit code of 1 through 254 prevents restarts.
-                  ExecCondition = pkgs.writeShellScript "check-metered-connection.sh" ''
-                    metered_status=$(${lib.getExe' pkgs.dbus "dbus-send"} --system --print-reply=literal \
-                      --system --dest=org.freedesktop.NetworkManager \
-                      /org/freedesktop/NetworkManager \
-                      org.freedesktop.DBus.Properties.Get \
-                      string:org.freedesktop.NetworkManager string:Metered \
-                      | ${lib.getExe pkgs.gnugrep} -o ".$")
+                # Syncing on a metered connection could be expensive.
+                # See <https://github.com/jdorel/systemd-metered-connection-dependency/blob/4a083159e20b444ac44340884d9563244ba91f7e/check-metered-connection.sh>
+                # for `check-metered-connection.sh`.
+                # Technically,
+                # this only stops the service from _starting_ while on a metered connection.
+                # In practice,
+                # the service should fail and restart
+                # when the connection changes.
+                # Note,
+                # an exit code of 1 through 254 prevents restarts.
+                ExecCondition = pkgs.writeShellScript "check-metered-connection.sh" ''
+                  metered_status=$(${lib.getExe' pkgs.dbus "dbus-send"} --system --print-reply=literal \
+                    --system --dest=org.freedesktop.NetworkManager \
+                    /org/freedesktop/NetworkManager \
+                    org.freedesktop.DBus.Properties.Get \
+                    string:org.freedesktop.NetworkManager string:Metered \
+                    | ${lib.getExe pkgs.gnugrep} -o ".$")
 
-                    if [[ $metered_status =~ (1|3) ]]; then
-                      exit 255
-                    else
-                      exit 0
-                    fi
-                  '';
-                };
-            };
-          })
-        ]
-      ) config.jstos.users;
-    }
-  )
+                  if [[ $metered_status =~ (1|3) ]]; then
+                    exit 255
+                  else
+                    exit 0
+                  fi
+                '';
+              };
+          };
+        })
+      ]
+    ) config.jstos.users;
+  })
 
   (lib.mkIf (builtins.any (cfg: cfg.enable) (builtins.attrValues snapshotCfgs)) {
     services.snapper = {
